7c89ff3eaac17d00386378ab320b5c8f42385076
Daniel Strong <daniel.strong@digital.hmrc.gov.uk>
2017-02-14 14:46:24 +0000

    Move provider into asg.tf

--- asg.tf
+++ asg.tf
@@ -3,6 +3,10 @@ variable "image_id" {
   default = "ami-bb373ddf"
 }
 
+provider "aws" {
+  region = "eu-west-2"
+}
+
 resource "aws_launch_configuration" "launch_configuration" {
   image_id      = "${var.image_id}"
   instance_type = "t2.micro"
