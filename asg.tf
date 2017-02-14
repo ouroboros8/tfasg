3ed284bfcd92cbf4592f4690be808d41a1335268
Daniel Strong <daniel.strong@digital.hmrc.gov.uk>
2017-02-14 11:35:24 +0000

    Inline the cloudformation for dependency reasons

--- asg.tf
+++ asg.tf
@@ -35,16 +35,26 @@ resource "aws_launch_configuration" "launch_configuration" {
 
 resource "aws_cloudformation_stack" "all_zones_asg" {
   name          = "my-asg"
-  template_body = "${data.template_file.cloudformation_auto_scaling_group.rendered}"
-  depends_on    = ["aws_launch_configuration.launch_configuration"]
-}
-
-data "template_file" "cloudformation_auto_scaling_group" {
-  template = "${file("cloudformation_auto_scaling_group.json.tpl")}"
-
-  vars {
-    launch_configuration = "${aws_launch_configuration.launch_configuration.name}"
-    max_size             = 2
-    min_size             = 2
+  template_body = <<EOF
+{
+  "Resources": {
+    "MyAsg": {
+      "Type": "AWS::AutoScaling::AutoScalingGroup",
+      "Properties": {
+        "AvailabilityZones": {"Fn::GetAZs": "eu-west-2"},
+        "LaunchConfigurationName": "${aws_launch_configuration.launch_configuration.name}",
+        "MaxSize": 2,
+        "MinSize": 2
+      },
+      "UpdatePolicy": {
+        "AutoScalingRollingUpdate": {
+          "MinInstancesInService": "2",
+          "MaxBatchSize": "1",
+          "PauseTime": "PT0S"
+        }
+      }
+    }
   }
+}
+EOF
 }
